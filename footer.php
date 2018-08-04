c0c9a41624b77a77448a71bb41954238fdc29450
Aaron Weidele <aweidele@gmail.com>
2018-08-04 17:51:31 -0400

    grid adjustments

--- footer.php
+++ footer.php
@@ -4,17 +4,17 @@
     <footer class="footer">
       <div class="footer_top">
         <div class="row">
-          <div class="col-3 push-1">
+          <div class="col-sm-2 col-md-2 col-lg-3 push-lg-1">
             <?php foreach($footer["footer_column_1"] as $block) {
               include("blocks/block_".$block["acf_fc_layout"].".php");
             } ?>
           </div>
-          <div class="col-3">
+          <div class="col-sm-2 col-md-2 col-lg-3 ">
             <?php foreach($footer["footer_column_2_footer_column_1"] as $block) {
               include("blocks/block_".$block["acf_fc_layout"].".php");
             } ?>
           </div>
-          <div class="col-4">
+          <div class="col-sm-2 col-md-4 col-lg-4">
             <nav class="footer_nav">
               <?php wp_nav_menu( array('theme_location' => 'footer-menu') ); ?>
             </nav>
@@ -23,10 +23,10 @@
       </div>
       <div class="footer_bottom">
         <div class="row">
-          <div class="col-3 push-1">
+          <div class="col-sm-2 col-md-2 col-lg-3 push-lg-1">
             © <?=date("Y")." ".$footer["copyright_info"]?>
           </div>
-          <div class="col-7">
+          <div class="col-sm-2 col-md-2 col-lg-7">
             <div class="footer_utility_nav">
 
               <?php wp_nav_menu( array('theme_location' => 'footer-utility-menu') ); ?>
