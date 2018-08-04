436582fedb61239ae43707029795259c49218bd0
Aaron Weidele <aweidele@gmail.com>
2018-08-04 17:31:30 -0400

    schema info

--- blocks/block_contact.php
+++ blocks/block_contact.php
@@ -1,31 +1,32 @@
 
-<div class="block_content">
-  <p class="address">
-    <?php if($block["address_line_1"]) { ?>
-    <span><?=$block["address_line_1"]?></span><br>
-    <?php } ?>
-    <?php if($block["address_line_2"]) { ?>
-    <span><?=$block["address_line_2"]?></span><br>
-    <?php } ?>
-    <?php if($block["city"]) { ?>
-    <span>
-      <span><?=$block["city"]?>, </span>
-      <?php if($block["state"]) { ?>
-      <span><?=$block["state"]?></span>
+<div class="block_contact" itemscope itemtype="http://schema.org/Organization">
+  <p class="schema_name" itemprop="name"><?=$block["schema_organization_name"]?></p>
+  <p class="address" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+    <span itemprop="streetAddress">
+      <?php if($block["address_line_1"]) { ?>
+      <span><?=$block["address_line_1"]?></span><br>
       <?php } ?>
-      <?php if($block["zip"]) { ?>
-      <span><?=$block["zip"]?></span>
+      <?php if($block["address_line_2"]) { ?>
+      <span><?=$block["address_line_2"]?></span><br>
       <?php } ?>
     </span>
+    <?php if($block["city"]) { ?>
+    <span itemprop="addressLocality"><?=$block["city"]?>, </span>
+    <?php } ?>
+    <?php if($block["state"]) { ?>
+    <span itemprop="addressRegion"><?=$block["state"]?></span>
+    <?php } ?>
+    <?php if($block["zip"]) { ?>
+    <span itemprop="postalCode"><?=$block["zip"]?></span>
     <?php } ?>
   </p>
   <?php if($block["phone_1"]) { ?>
-  <p class="phone"><a href="tel:<?=str_replace(array("(",")"," ","-"),"",$block["phone_1"])?>"><?=$block["phone_1"]?></a></p>
+  <p class="phone"><a href="tel:<?=str_replace(array("(",")"," ","-"),"",$block["phone_1"])?>" itemprop="telephone"><?=$block["phone_1"]?></a></p>
   <?php } ?>
   <?php if($block["phone_2"]) { ?>
-  <p class="phone"><a href="tel:<?=str_replace(array("(",")"," ","-"),"",$block["phone_2"])?>"><?=$block["phone_2"]?></a></p>
+  <p class="phone"><a href="tel:<?=str_replace(array("(",")"," ","-"),"",$block["phone_2"])?>" itemprop="telephone"><?=$block["phone_2"]?></a></p>
   <?php } ?>
   <?php if($block["email"]) { ?>
-  <p class="email"><a href="mainto:<?=$block["email"]?>"><?=$block["email"]?></a></p>
+  <p class="email"><a href="mainto:<?=$block["email"]?>" itemprop="email"><?=$block["email"]?></a></p>
   <?php } ?>
 </div>
