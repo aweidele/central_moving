c91ede1fe48d8a771590e9edf7fd337abaf288b0
Aaron Weidele <aweidele@gmail.com>
2018-08-14 07:29:22 -0400

    style the homepage hero

--- blocks/block_hero.php
+++ blocks/block_hero.php
@@ -1,12 +1,26 @@
 <div class="hero_wrapper">
-  <div class="hero">
+  <div class="hero owl-carousel" data-options='{"items":1}'>
     <?php
       foreach($hero as $slide) {
     ?>
     <div class="hero_item">
       <figure class="hero_image">
-        <img src="<?php echo $slide["hero_image"]["sizes"]["hero-image-sm"]; ?>">
+        <img src="<?=$slide["hero_image"]["sizes"]["hero-image-sm"]; ?>">
       </figure>
+      <div class="hero_content">
+        <div class="row">
+          <div class="col_xl_8 push_xl_1">
+            <div class="hero_copy">
+              <?=wpautop($slide["hero_copy"])?>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col_xl_8 push_xl_1">
+            <a href="<?=$slide["hero_link"]["url"]?>" class="hero_cta"><?=$slide["hero_link"]["title"] ? $slide["hero_link"]["title"] : "Read More"?></a>
+          </div>
+        </div>
+      </div>
     </div>
     <?php
       }
--- js/scripts.js
+++ js/scripts.js
@@ -3451,4 +3451,9 @@
   $("#header_nav_button").on("click",function() {
     $("body").toggleClass("menu_open");
   });
+
+  $(".owl-carousel").each(function() {
+    var options = $(this).data("options");
+    $(this).owlCarousel(options);
+  });
 })(jQuery);
