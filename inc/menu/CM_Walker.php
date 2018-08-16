acdf0d8c40e76970c0df25546e3c28db95be433c
R2INTEGRATED\aweidele <aweidele@r2integrated.com>
2018-08-16 16:30:10 -0400

    expanding menu on mobile

--- inc/menu/CM_Walker.php
+++ inc/menu/CM_Walker.php
@@ -2,31 +2,33 @@
   /* https://core.trac.wordpress.org/browser/tags/4.9.8/src//wp-includes/class-walker-nav-menu.php#L0 */
   class CM_Walker extends Walker_Nav_Menu {
 
-    // Displays start of an element. E.g '<li> Item Name'
-    // @see Walker::start_el()
-    function start_el(&$output, $item, $depth=0, $args=array(), $id = 0) {
-      $object = $item->object;
-      $type = $item->type;
-      $title = $item->title;
-      $description = $item->description;
-      $permalink = $item->url;
-      $output .= "<li class='" .  implode(" ", $item->classes) . "'>";
+    public function start_lvl( &$output, $depth = 0, $args = array() ) {
+            if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
+                    $t = '  ';
+                    $n = '';
+            } else {
+                    $t = "\t";
+                    $n = "\n";
+            }
+            $indent = str_repeat( $t, $depth );
 
-      //Add SPAN if no Permalink
-      if( $permalink && $permalink != '#' ) {
-        $output .= '<a href="' . $permalink . '">';
-      } else {
-        $output .= '<span>';
-      }
+            // Default class.
+            $classes = array( 'sub-menu' );
 
-      $output .= $title;
-      if( $description != '' && $depth == 0 ) {
-        $output .= '<small class="description">' . $description . '</small>';
-      }
-      if( $permalink && $permalink != '#' ) {
-        $output .= '</a>';
-      } else {
-        $output .= '</span>';
-      }
+            /**
+             * Filters the CSS class(es) applied to a menu list element.
+             *
+             * @since 4.8.0
+             *
+             * @param array    $classes The CSS classes that are applied to the menu `<ul>` element.
+             * @param stdClass $args    An object of `wp_nav_menu()` arguments.
+             * @param int      $depth   Depth of menu item. Used for padding.
+             */
+            $class_names = join( ' ', apply_filters( 'nav_menu_submenu_css_class', $classes, $args, $depth ) );
+            $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+            $output .= '<button class="main_nav_expand">Expand</button>';
+            $output .= "{$n}{$indent}<ul$class_names>{$n}";
     }
+
   }
--- js/scripts.js
+++ js/scripts.js
@@ -3452,6 +3452,15 @@
     $("body").toggleClass("menu_open");
   });
 
+  $(".main_nav_expand").on("click",function() {
+    if( $(this).parent().hasClass("expanded") ) {
+      $(this).parent().removeClass("expanded");
+    } else {
+      $(".main_nav .menu-item").removeClass("expanded");
+      $(this).parent().addClass("expanded");
+    }
+  });
+
   $(".owl-carousel").each(function() {
     var options = $(this).data("options");
     $(this).owlCarousel(options);
