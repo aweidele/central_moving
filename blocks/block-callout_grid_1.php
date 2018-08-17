51cdd1bed60ec7a2ad8aac3f62c25e54f206392e
R2INTEGRATED\aweidele <aweidele@r2integrated.com>
2018-08-17 16:54:43 -0400

    callout grid 1

---
+++ blocks/block-callout_grid_1.php
@@ -0,0 +1,19 @@
+
+<section class="callout_grid_1">
+  <div class="row">
+    <div class="col_md_9 push_lg_1">
+      <div class="callout_grid_wrapper">
+        <header>
+          <h2><?=$block["title"]?></h2>
+        </header>
+        <div class="callout_grid_1_inner">
+          <p class="callout_intro">
+            <?=nl2br($block["description"])?>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<pre><?php print_r($block); ?></pre>
