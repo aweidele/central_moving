0ba1afd9cbad3f048d93113241d3233dcd34f3ee
Aaron Weidele <aweidele@gmail.com>
2018-08-19 15:32:39 -0400

    style grid #2

--- blocks/block-callout_grid_1.php
+++ blocks/block-callout_grid_1.php
@@ -1,28 +1,41 @@
+<?php
+  if(!$grid_image_sz1) {
+    $grid_image_sz1 = "grid-image-443";
+  }
 
-<div class="row">
-  <div class="col_md_9 push_lg_1">
-    <section class="callout_grid grid_1">
+  if(!$grid_image_sz2) {
+    $grid_image_sz2 = "grid-image-546";
+  }
+?>
+<section class="callout_grid <?=$block["acf_fc_layout"]?>">
+  <div class="row">
+    <div class="col_md_9 push_lg_1">
+
       <div class="callout_grid_wrapper">
         <header class="callout_grid_header">
           <h2><?=$block["title"]?></h2>
         </header>
         <div class="callout_grid_inner">
+          <?php if($block["subtitle"]) { ?>
+          <h3 class="callout_grid_subhead"><?=$block["subtitle"]?></h3>
+          <?php } ?>
           <div class="callout_grid_intro">
             <div class="callout_grid_intro_inner">
               <?=$block["description"]?>
             </div>
           </div>
           <figure class="callout_grid_image callout_grid_image_1">
-            <img src="<?=$block["image_1"]["sizes"]["grid-image-443"]?>">
+            <img src="<?=$block["image_1"]["sizes"][$grid_image_sz1]?>">
           </figure>
           <figure class="callout_grid_image callout_grid_image_2">
-            <img src="<?=$block["image_2"]["sizes"]["grid-image-546"]?>">
+            <img src="<?=$block["image_2"]["sizes"][$grid_image_sz2]?>">
           </figure>
           <div class="callout_grid_cta">
             <a href="<?=$block["link"]["url"]?>"><?=$block["link"]["title"] ? $block["link"]["title"] : "Read More"?></a>
           </div>
         </div>
       </div>
-    </section>
+
+    </div>
   </div>
-</div>
+</section>
--- blocks/block-callout_grid_2.php
+++ blocks/block-callout_grid_2.php
@@ -1,28 +1,4 @@
-
-<div class="row">
-  <div class="col_md_9 push_lg_1">
-    <section class="callout_grid grid_1">
-      <div class="callout_grid_wrapper">
-        <header class="callout_grid_header">
-          <h2><?=$block["title"]?></h2>
-        </header>
-        <div class="callout_grid_inner">
-          <div class="callout_grid_intro">
-            <div class="callout_grid_intro_inner">
-              <?=$block["description"]?>
-            </div>
-          </div>
-          <figure class="callout_grid_image callout_grid_image_1">
-            <img src="<?=$block["image_1"]["sizes"]["grid-image-443"]?>">
-          </figure>
-          <figure class="callout_grid_image callout_grid_image_2">
-            <img src="<?=$block["image_2"]["sizes"]["grid-image-546"]?>">
-          </figure>
-          <div class="callout_grid_cta">
-            <a href="<?=$block["link"]["url"]?>"><?=$block["link"]["title"] ? $block["link"]["title"] : "Read More"?></a>
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
-</div>
+<?php
+$grid_image_sz1 = "grid-image-300";
+$grid_image_sz2 = "grid-image-500";
+include("block-callout_grid_1.php");
