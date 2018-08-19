8e0361dc73603063a5b5b06a55deb19f032bc9c2
Aaron Weidele <aweidele@gmail.com>
2018-08-19 13:33:54 -0400

    styling callout

--- blocks/block-callout_grid_1.php
+++ blocks/block-callout_grid_1.php
@@ -1,19 +1,28 @@
 
-<section class="callout_grid_1">
-  <div class="row">
-    <div class="col_md_9 push_lg_1">
+<div class="row">
+  <div class="col_md_9 push_lg_1">
+    <section class="callout_grid grid_1">
       <div class="callout_grid_wrapper">
-        <header>
+        <header class="callout_grid_header">
           <h2><?=$block["title"]?></h2>
         </header>
-        <div class="callout_grid_1_inner">
-          <p class="callout_intro">
+        <div class="callout_grid_inner">
+          <p class="callout_grid_intro">
             <?=nl2br($block["description"])?>
           </p>
+          <figure class="callout_grid_image callout_grid_image_1">
+            <img src="<?=$block["image_1"]["sizes"]["grid-image-443"]?>">
+          </figure>
+          <figure class="callout_grid_image callout_grid_image_2">
+            <img src="<?=$block["image_2"]["sizes"]["grid-image-546"]?>">
+          </figure>
+          <div class="callout_grid_cta">
+            <a href="<?=$block["link"]["url"]?>"><?=$block["link"]["title"] ? $block["link"]["title"] : "Read More"?></a>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   </div>
-</section>
+</div>
 
 <pre><?php print_r($block); ?></pre>
