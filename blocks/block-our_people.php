c8938cb7c1f99149e5e628bd93795248948c5ac1
Aaron Weidele <aweidele@gmail.com>
2018-08-26 11:38:01 -0400

    setting up our people block

---
+++ blocks/block-our_people.php
@@ -0,0 +1,15 @@
+<section class="our_people" id="our-people">
+  <div class="row">
+    <div class="col_md_4 col_lg_9 col_xl_6 push_xl_1">
+      <header>
+        <h2 class="our_people_heading"><?=$block["title"]?></h2>
+      </header>
+      <div class="our_people_description"><?=$block["description"]?></div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col_md_4 col_lg_11 col_xl_9 push_xl_1">
+      
+    </div>
+  </div>
+</section>
---
+++ page.php
@@ -0,0 +1,12 @@
+<?php
+  get_header();
+  $fields = get_fields();
+  // $hero = $fields["homepage_hero"];
+  // $heroSpeed = $fields["homepage_hero_speed"];
+  // include("blocks/block-hero.php");
+?>
+
+<main class="main">
+<?php cm_blocks($fields["blocks"]); ?>
+</main>
+<?php get_footer(); ?>
