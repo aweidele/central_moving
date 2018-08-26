5ecb2906f39812020ff3b1d06b83971afa35fd31
Aaron Weidele <aweidele@gmail.com>
2018-08-26 09:08:25 -0400

    Bio page right hand info

--- single-our-people.php
+++ single-our-people.php
@@ -13,14 +13,59 @@
         <figure class="our_people_thumb">
           <?=$thumbnail?>
         </figure>
-        <div class="our_people_contact">
-          <dl>
-            <li><strong>Email</strong>
+        <div class="our_people_info">
+
+          <?php if($fields["email"] || $fields["main_phone"] || $fields["direct_phone"]) { ?>
+          <dl class="our_people_contact">
+            <?php if($fields["email"]) { ?>
+
+            <dt>Email</dt>
+            <dd><a href="mailto:<?=$fields["email"]?>"><?=$fields["email"]?></a></dd>
+
+            <?php }
+            if($fields["main_phone"]) { ?>
+
+            <dt>Main</dt>
+            <dd><a href="tel:<?=str_replace(["(",")"," ","-"],"",$fields["main_phone"])?>"><?=$fields["main_phone"]?></a></dd>
+
+            <?php }
+            if($fields["direct_phone"]) { ?>
+
+            <dt>Direct</dt>
+            <dd><a href="tel:<?=str_replace(["(",")"," ","-"],"",$fields["direct_phone"])?>"><?=$fields["direct_phone"]?></a></dd>
+
+            <?php } ?>
           </dl>
+          <?php }
+          if($fields["education"]) { ?>
+
+          <dl>
+            <dt>Education</dt>
+            <dd><?=nl2br($fields["education"])?></dd>
+          </dl>
+
+          <?php }
+          if($fields["affiliations"]) { ?>
+
+          <dl>
+            <dt>Affiliations</dt>
+            <dd><?=nl2br($fields["affiliations"])?></dd>
+          </dl>
+
+          <?php }
+          if($fields["connect"]) { ?>
+
+          <ul>
+            <?php foreach($fields["connect"] as $connect) { ?>
+            <li><?=$connect["connect_text"]?> <a href="<?=$connect["link"]["url"]?>" target="_blank"><?=$connect["link"]["title"]?></a></li>
+            <?php } ?>
+          </ul>
+
+          <?php } ?>
+
         </div>
       </div>
     </div>
-    <pre><?php print_r($fields); ?></pre>
   </main>
 <?php
   endwhile;
