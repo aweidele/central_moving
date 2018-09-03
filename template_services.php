9c2a60a0f0d43e583a825ab3e41ba744e68e99c3
Aaron Weidele <aweidele@gmail.com>
2018-09-03 13:54:20 -0400

    services navigation

--- template_services.php
+++ template_services.php
@@ -1,6 +1,12 @@
 <?php
 /* Template Name: Services */
 get_header();
+$services = get_posts([
+  "post_type" => "services",
+  "orderby" => "menu_order",
+  "posts_per_page" => -1
+]);
+
 if( have_posts() ): while( have_posts() ): the_post();
   $fields = get_fields();
   $hero = $fields["hero_image"];
@@ -9,7 +15,7 @@ if( have_posts() ): while( have_posts() ): the_post();
 ?>
   <main class="main services_main">
     <div class="row reverse">
-      <div class="col_md_7 col_xl_8">
+      <div class="col_11 col_lg_7 col_xl_8">
         <div class="services_content_wrapper">
           <div class="services_content">
             <h1><?=$fields["title"] ? $fields["title"] : get_the_title()?></h1>
@@ -39,10 +45,19 @@ if( have_posts() ): while( have_posts() ): the_post();
           </div>
         </div>
       </div>
-      <div class="col_md_4 col_xl_3">
-        Sidebar
+      <div class="col_11 col_lg_4 col_xl_3">
+        <nav class="services_nav">
+          <ul>
+            <li class="active"><a href=""><span>Services</span></a></li>
+            <?php foreach($services as $service) { ?>
+            <li><a href="<?=get_permalink($service->ID)?>"><span><?=$service->post_title?></span></a></li>
+            <?php } ?>
+          </ul>
+        </nav>
       </div>
     </div>
+
+    <pre><?php print_r($services); ?></pre>
   </main>
   <?php cm_blocks($fields["blocks"]); ?>
 <?php
