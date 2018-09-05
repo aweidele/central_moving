6f69aa3384573ab4953e09e39b989b9490eb4b15
Aaron Weidele <aweidele@gmail.com>
2018-09-05 18:59:24 -0400

    full width text and image

--- blocks/block-text_and_image_two_col.php
+++ blocks/block-text_and_image_two_col.php
@@ -1,1 +1,13 @@
-<pre><?php print_r($block); ?></pre>
+<section class="<?=$block["acf_fc_layout"]?>">
+  <div class="row">
+      <div class="col col_lg_4 push_xl_1">
+        <h2><?=$block["copy"]["title"]?></h2>
+        <?=wpautop($block["copy"]["copy"])?>
+      </div>
+      <div class="col col_lg_6">
+        <figure>
+          <img src="<?=$block["image"]["sizes"]["grid-image-700"]?>" alt="<?=$block["image"]["alt"]?>">
+        </figure>
+      </div>
+  </div>
+</section>
