44cbfebeff272a7d5c732fc2d062888ab2d4fda4
Aaron Weidele <aweidele@gmail.com>
2018-09-05 20:32:18 -0400

    allow for multiple testimonials

--- blocks/block-testimonial.php
+++ blocks/block-testimonial.php
@@ -6,10 +6,18 @@
     </header>
     <div class="row">
       <div class="col_sm_2 col_md_2 col_lg_7 push_xl_1">
-        <blockquote>
-          <?=nl2br($block["testimonial"])?>
-        </blockquote>
-        <cite><?=$block["attribution"]?></cite>
+        <div class="testimonials<?php if( sizeof($block["testimonials"]) > 1 ) { echo " owl-carousel"; } ?>" data-options='{"items":1,"loop":true,"animateIn": "fadeIn","animateOut": "fadeOut","autoplay":true,"autoplayTimeout":<?=$block["speed"]?>}'>
+          <?php foreach($block["testimonials"] as $testimonial) { ?>
+
+          <div class="testimonial_item">
+            <blockquote>
+              <?=nl2br($testimonial["testimonial"])?>
+            </blockquote>
+            <cite><?=$testimonial["attribution"]?></cite>
+          </div>
+
+          <?php } ?>
+        </div>
       </div>
       <div class="col_sm_2 col_md_2 col_lg_3">
         <a class="testimonial_cta" href="<?=$block["cta"]["url"]?>"<?php if($block["cta"]["target"]) { echo ' target="'.$block["cta"]["target"] .'"'; } ?>><?=$block["cta"]["title"]?></a>
