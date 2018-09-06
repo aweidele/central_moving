309b797750d189bc9179e6877d3455d356e6493c
Aaron Weidele <aweidele@gmail.com>
2018-09-05 21:41:43 -0400

    Multicolumn List

--- blocks/block-multicolumn_list.php
+++ blocks/block-multicolumn_list.php
@@ -1,1 +1,14 @@
-<pre><?php print_r($block) ?></pre>
+<?php
+$list = explode("\n", $block["list"]);
+?>
+<section class="multicolumn_list">
+  <div class="row">
+    <div class="col_11 col_xl_9 push_xl_1">
+      <ul class="multicolumn_list_list list_<?=$block["columns"]?>_column">
+        <?php foreach($list as $item) { ?>
+        <li><?=$item?></li>
+        <?php } ?>
+      </ul>
+    </div>
+  </div>
+</section>
