e9c12ea333be720269ce84ad4546014fe087ce0d
AARON WEIDELE <aaronweidele@AARONs-MacBook.local>
2018-10-01 22:31:21 -0400

    featured case studies

--- blocks/block-featured_case_studies.php
+++ blocks/block-featured_case_studies.php
@@ -7,27 +7,39 @@
         </header>
         <?php
           foreach( $block["featured_case_studies_right"] as $case_study ) {
-            $thumbnail = get_the_post_thumbnail($case_study->ID, "case_study_listing");
-        ?>
-        <article class="case_study">
-          <a href="<?=get_permalink($case_study->ID)?>"><?=$thumbnail?></a>
-        </article>
-        <?php
+            cs_block($case_study);
           }
         ?>
+        <a href="<?=$block["link"]["url"]?>" class="featured_case_studies_link right" aria-hidden="true"><?=$block["link"]["title"] ? $block["link"]["title"] : "Case Studies" ?></a>
       </div>
     </div>
-    <div class="col_11 col_lg_5 col_xl_4">
+    <div class="col_11 col_lg_5 col_xl_4 push_xl_1">
       <?php
         foreach( $block["featured_case_studies_left"] as $case_study ) {
-          $thumbnail = get_the_post_thumbnail($case_study->ID, "case_study_listing");
-      ?>
-      <article class="case_study">
-        <a href="<?=get_permalink($case_study->ID)?>"><?=$thumbnail?></a>
-      </article>
-      <?php
+          cs_block($case_study);
         }
       ?>
+      <a href="<?=$block["link"]["url"]?>" class="featured_case_studies_link left"><?=$block["link"]["title"] ? $block["link"]["title"] : "Case Studies" ?></a>
     </div>
   </div>
 </section>
+<?php
+  function cs_block($case_study) {
+    $thumbnail = get_the_post_thumbnail($case_study->ID, "case_study_listing");
+    $terms = wp_get_post_terms($case_study->ID, "industry");
+?>
+
+        <article class="case_study">
+          <a href="<?=get_permalink($case_study->ID)?>">
+            <figure class="featured_case_study_figure"><?=$thumbnail?></figure>
+            <ul class="case_study_terms">
+              <?php foreach($terms as $term) { ?>
+              <li><?=$term->name?></li>
+              <?php } ?>
+            </ul>
+            <h2><?=$case_study->post_title?></h2>
+          </a>
+        </article>
+
+<?php
+  }
