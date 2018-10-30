49d74070392735868f194f4a2bafc2b488b494a2
AARON WEIDELE <aaronweidele@AARONs-MacBook.local>
2018-10-30 07:48:13 -0400

    add id field

--- blocks/block-text_and_image_two_col.php
+++ blocks/block-text_and_image_two_col.php
@@ -1,4 +1,4 @@
-<section class="<?=$block["acf_fc_layout"]?>">
+<section class="<?=$block["acf_fc_layout"]?>"<?php if($block["id"]) { echo ' id="'.$block["id"].'"'; } ?>>
   <div class="row">
       <div class="col col_lg_4 push_xl_1">
         <h2><?=$block["copy"]["title"]?></h2>
