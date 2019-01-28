fb2ae200cc1a317d9e6d91754acda146afb4245a
Aaron Weidele <aweidele@gmail.com>
2019-01-28 15:00:11 -0700

    Changes on server

--- blocks/block-hero.php
+++ blocks/block-hero.php
@@ -5,8 +5,8 @@
       foreach($hero as $slide) {
     ?>
     <div class="hero_item">
-      <figure class="hero_image">
-        <img src="<?=$slide["hero_image"]["sizes"]["hero-image-sm"]; ?>">
+      <figure class="hero_image focal-center">
+	  <img src="<?=$slide["hero_image"]["url"]; ?>"  data-width="<?=$slide["hero_image"]["width"]?>" data-height="<?=$slide["hero_image"]["height"]?>">
       </figure>
       <div class="hero_content">
         <div class="row">
@@ -29,10 +29,47 @@
       }
     ?>
   </div>
-  <div class="hero_scroll">
+  <script>
+		jQuery(function(){
+		function center() {
+			var images = jQuery(".focal-center img");
+			var $window = jQuery(".hero_carousel");
+			var windowWidth = $window.width();
+			var windowHeight = $window.height();
+			var windowRatio = windowWidth / windowHeight;
+			
+			images.each(function(){
+				var $this = jQuery(this);
+				var width = parseFloat($this.data("width"));
+				var height = parseFloat($this.data("height"));
+				var ratio = width / height;
+				
+				if( windowRatio > ratio ) {
+					var newHeight = (windowWidth / ratio);
+					var newTop = (newHeight - windowHeight) / 2;
+					
+					if( newTop > 0 ) {
+						newTop *= -1;
+					}
+					$this.css({ height: newHeight, width: windowWidth, left: 0, top: newTop });
+				} else {
+					var newWidth = (windowHeight * ratio);
+					var newLeft = (newWidth - windowWidth) / 2;
+					
+					if( newLeft > 0 ) {
+						newLeft *= -1;
+					}
+					$this.css({ height: windowHeight, width: newWidth, top: 0, left: newLeft });
+				}
+			});
+		}
+		center();
+		jQuery(window).on("resize.blockHero", center);
+		});
+	</script>
+  <div class="hero_scroll" style>
     <div class="row">
       <a href="#main">
-        <span>Scroll Down</span>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 99.66 27.51"><title>Scroll</title><path d="M49.83,27.51a3.47,3.47,0,0,1-1.41-.3L2.08,6.7A3.5,3.5,0,0,1,4.92.3L49.83,20.18,94.75.3a3.5,3.5,0,0,1,2.83,6.4L51.25,27.21A3.52,3.52,0,0,1,49.83,27.51Z"/></svg>
       </a>
     </div>
